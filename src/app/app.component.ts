97fc32f1df267727e76674abaecbe1a1872532ee
duhaali <dohaali415@gmail.com>
2017-12-13 13:36:31 +0200

    (edit)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Geolocation } from '@ionic-native/geolocation';
 
-import {MapPage} from '../pages/map/map';
 import { HomePage } from '../pages/home/home';
+import {MapPage} from '../pages/map/map';
+import { SignupPage } from '../pages/signup/signup';
+import { SigninPage } from '../pages/signin/signin';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
 
-rootPage:any = MapPage;
+// rootPage:any = MapPage;
+rootPage:any = SignupPage;
   @ViewChild(Nav) nav: Nav;
 
   constructor(platform: Platform, 
