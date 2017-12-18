97d8e6d8b01013253695c676c52b70a84ebaa56f
Rana Kelani <rana.kelani@arabiaweather.com>
2017-12-19 01:23:18 +0200

    search for users

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import { UsersProvider } from '../../providers/users/users';
+import { Subscription } from 'rxjs/Subscription';
 import { PersonInfoPage } from '../person-info/person-info';
 
 
@@ -10,6 +11,9 @@ import { PersonInfoPage } from '../person-info/person-info';
  templateUrl: 'chat.html',
 })
 export class ChatPage {
+  private usersObservable: Subscription;
+  users = [];
+  searchResults = [];
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     private _authProvider: AuthProvider,
@@ -18,11 +22,22 @@ export class ChatPage {
   }
   getUsers(){
     this.usersObservable = this. _usersProvider.getAttendees().subscribe(users => {
-      this.users = users;
+      this.searchResults = this.users = users;
     });
   }
-
-  
+  findUsers(ev){
+    var query = ev.target.value.toLowerCase();
+    var keys = ['firstName', 'lastName'];
+   
+    if(query && query.trim() != ''){
+      this.users = this.searchResults.filter((item) => {
+        return keys.some( key => 
+          String(item[key]).toLowerCase().includes(query));
+      });
+    }else{
+      this.getUsers();
+    }
+  }
 
   showPerson(person) {
     this.navCtrl.push(PersonInfoPage, {person: person});
