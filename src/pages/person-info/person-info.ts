7786c65264d6f3195060206cd8bad7e0636d282d
SirajKakeh <sirajonline.89@gmail.com>
2017-12-23 22:53:39 +0200

    (add) person's info page

--- src/pages/person-info/person-info.ts
+++ src/pages/person-info/person-info.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { AuthProvider } from '../../providers/auth/auth';
-import { AttendeesPage } from '../attendees/attendees';
 
 @Component({
   selector: 'page-person-info',
   templateUrl: 'person-info.html',
 })
 
-export class PersonInfoPage {  
+export class PersonInfoPage {
+  person;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams) {
+      this.person = this.navParams.get('person');
   }
 }
