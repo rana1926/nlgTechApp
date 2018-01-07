7f5c92108122b03e8d0b759e34e02245a8a7c8b4
SirajKakeh <sirajonline.89@gmail.com>
2018-01-08 00:08:46 +0200

    build configuration

--- src/pages/splash-screen/splash-screen.ts
+++ src/pages/splash-screen/splash-screen.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
 
-@IonicPage()
 @Component({
   selector: 'page-splash-screen',
   templateUrl: 'splash-screen.html',
